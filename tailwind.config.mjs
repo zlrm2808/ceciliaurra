9d980c14cba2e7c2c71945c6b94507c641bf9bd0
Zeus Rodriguez <zlrm2808@gmail.com>
2024-11-04 05:20:17 -0400

    Responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+	integrations: [tailwind()],
+});
