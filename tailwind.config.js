242ef92ebc4d80b70b50db1ed6a6bcafeb32c7b0
Zeus Rodriguez <zlrm2808@gmail.com>
2024-11-27 08:24:29 -0400

    eliminacion de lo que no hacia falta

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+	content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
+
+export default config;
+
+
+/*
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
 	integrations: [tailwind()],
 });
+
+*/
